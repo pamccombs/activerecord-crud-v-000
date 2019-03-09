f3e462ed5e8361a61439cf12cd323fa1828a24ce
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-09 13:17:52 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -89,7 +89,7 @@ def can_update_multiple_items_at_once
 end
 
 def can_destroy_a_single_item
-  #Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
+  Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   delete_1 = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
   delete_1.delete
   
