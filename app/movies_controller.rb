9ddc7528b2123d9bcc381f903d30ae8e62d0277a
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-09 12:58:22 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,8 +31,8 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   
   
   Movie.create do |m|
-    m.title = 
-    m.release_date = 
+    m.title = args[]
+    m.release_date = args[]
     m.save
   end
 end
