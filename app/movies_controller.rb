5b683a1dfabc6dc31d0dcf48b9df80bb20c01edc
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-09 13:32:28 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -79,6 +79,7 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   update_1 = Movie.create(title: "Wat?")
   update_1.update(title: "Wat, huh?")
+  ##
 end
 
 def can_update_multiple_items_at_once
